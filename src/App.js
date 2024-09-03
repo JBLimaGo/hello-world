5f90b7ad79fc39ac99bc6016bc176b11a9aef694
Jefferson Lima <jbldesenvolvimento@gmail.com>
2024-09-03 09:23:50 -0300

    a

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1> Hello World React!</h1>
+      <h1> Hello Bora Codar!</h1>
       <h2>Emment funcionou</h2>
-          
-          
-          
-          <h1>JBLima</h1>
+
+      <h1>JBLima</h1>
     </div>
   );
 }
